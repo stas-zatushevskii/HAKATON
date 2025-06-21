de6dc972655fb0c4541e26226ea1f05a2e092599
TurcanS <extremg8@gmail.com>
2025-06-21 22:33:42 +0300

    Fixed issue with the API

--- hakaton/hakaton/urls.py
+++ hakaton/hakaton/urls.py
@@ -21,5 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('', include('users.urls')),
+    path('learning/', include('users.urls')),
 ]
