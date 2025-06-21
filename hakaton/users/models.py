72187fa712ea4ae35cd435e9ba4241cdf22d44ee
Стас Затушевский <STAs20160@yandex.ru>
2025-06-21 20:48:08 +0300

    refactored models

--- hakaton/users/models.py
+++ hakaton/users/models.py
@@ -1,38 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
-class Teacher(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length=120, null=True, blank=True)
-
-class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length=120, null=True, blank=True)
-
-class TaskType(models.Model):
-    name = models.CharField(max_length=120)
-
-
-class Task(models.Model):
+class Topic(models.Model):
     id = models.UUIDField(primary_key=True, editable=False)
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-    task_type = models.ForeignKey(TaskType, on_delete=models.SET_NULL, null=True)
-    name = models.CharField(max_length=120)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    due_date = models.DateTimeField(null=True, blank=True)
-    solution = models.CharField(max_length=500, default="")
-    sample = models.CharField(max_length=10000, default="")
+    name = models.CharField(max_length=255)
+    level = models.CharField(max_length=255)
+    tone = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topics')
 
-
-class TaskAssignment(models.Model):
-    name = models.CharField(max_length=120) # unique name for each solution from student, will form like this - task_id+task_name+student_id
-    task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    status = models.CharField(max_length=20, choices=[
-        ("pending", "Ожидает"),
-        ("submitted", "Отправлено"),
-        ("checked", "Проверено")
-    ])
-    submitted_at = models.DateTimeField(null=True, blank=True)
+class Question(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False)
+    text = models.TextField()
+    answer = models.TextField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='questions')
