8fcf85b227e23e0395969192b0b98540072aaf5c
TurcanS <extremg8@gmail.com>
2025-06-21 18:51:42 +0300

    Added a login and registare form (work in proggress)

--- hakaton/hakaton/views.py
+++ hakaton/hakaton/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
