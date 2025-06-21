cc2a00c94369e3fac1331aaa68ba69dbbf8c2a17
TurcanS <extremg8@gmail.com>
2025-06-21 21:05:06 +0300

    Implemented user authentication features: added login and logout views, integrated custom forms for login and registration, and updated templates to reflect authentication state. Enhanced error handling and success messages for user feedback.

--- hakaton/hakaton/urls.py
+++ hakaton/hakaton/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
+    path('logout/', views.logout, name='logout'),
 ]
