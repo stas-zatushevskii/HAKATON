12dc1dcd9bede0ca670f470895266658e7f6843d
Стас Затушевский <STAs20160@yandex.ru>
2025-06-21 15:08:00 +0300

    added prototype of models

--- hakaton/users/models.py
+++ hakaton/users/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.postgres.fields import JSONField  # если используешь PostgreSQL
 
-# Create your models here.
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=120)
+
+
+class Task(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    task_type = models.ForeignKey(TaskType, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+
+
+class TaskAssignment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    solution = models.JSONField(default=dict)
+    status = models.CharField(max_length=20, choices=[
+        ("pending", "Ожидает"),
+        ("submitted", "Отправлено"),
+        ("checked", "Проверено")
+    ])
+    submitted_at = models.DateTimeField(null=True, blank=True)
