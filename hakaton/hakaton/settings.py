8004f89d7613ab69bb4ef10a5763828f50ebc6eb
TurcanS <extremg8@gmail.com>
2025-06-22 09:54:07 +0300

    Final Commit

--- hakaton/hakaton/settings.py
+++ hakaton/hakaton/settings.py
@@ -22,15 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-h7tglx9)$@*h^k9&fz1jmtxn(b-i6&o-b&7!#$!z-1udi0eg%6')
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-here-change-in-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
-
-ALLOWED_HOSTS = ['left4dead.tech', 'www.left4dead.tech', '127.0.0.1', 'localhost', '165.232.122.1']
-
-# CSRF trusted origins for HTTPS
-CSRF_TRUSTED_ORIGINS = ['https://left4dead.tech', 'https://www.left4dead.tech']
 
 # Application definition
 
@@ -139,18 +134,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Authentication settings
-LOGIN_URL = '/login/'
-LOGIN_REDIRECT_URL = '/'
-
-# Security settings for production
-if not DEBUG:
-    SECURE_BROWSER_XSS_FILTER = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_REDIRECT_EXEMPT = []
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
